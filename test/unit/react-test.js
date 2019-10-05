40f6c264c051bf1ab76d80b2525ae6601579553e
Matt Travi <programmer@travi.org>
2019-10-05 17:07:09 -0500

    test(form8ion): removed assertions covered by extending form8ions preset

--- test/unit/react-test.js
+++ test/unit/react-test.js
@@ -5,9 +5,7 @@ suite('react', () => {
   test('that react is configured when specified', () => {
     const config = buildConfig(null, {react: true});
 
-    assert.include(config.presets, require('@babel/preset-react'));
     assert.include(config.presets, '@emotion/babel-preset-css-prop');
-    assert.include(config.plugins, require('@babel/plugin-proposal-class-properties'));
     assert.include(config.plugins, require('babel-plugin-inline-react-svg').default);
     assert.include(config.plugins, 'polished');
   });
@@ -15,9 +13,7 @@ suite('react', () => {
   test('that react is not configured when not specified', () => {
     const config = buildConfig();
 
-    assert.notInclude(config.presets, require('@babel/preset-react'));
     assert.notInclude(config.presets, '@emotion/babel-preset-css-prop');
-    assert.notInclude(config.plugins, require('@babel/plugin-proposal-class-properties'));
     assert.notInclude(config.plugins, require('babel-plugin-inline-react-svg').default);
     assert.notInclude(config.plugins, 'polished');
   });
@@ -25,7 +21,6 @@ suite('react', () => {
   test('that emotion is disabled when specified', () => {
     const config = buildConfig(null, {react: true, emotion: false});
 
-    assert.include(config.presets, require('@babel/preset-react'));
     assert.notInclude(config.presets, '@emotion/babel-preset-css-prop');
   });
 });
